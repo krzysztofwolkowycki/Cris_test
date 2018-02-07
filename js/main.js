f76a6a362f0c3f060fd6d0dca31143c0e079ce78
Krzysztof Wolkowycki <wolkowycki.k@gmail.com>
2018-02-07 19:33:29 +0100

    Exercises 2_2a+Hello Word

---
+++ js/main.js
@@ -0,0 +1,40 @@
+
+document.getElementById("go").onclick = function() {
+    var name = document.getElementById("name").value;
+    var el = document.getElementById("men");
+    el.textContent = name +" " + "!";
+}
+
+document.getElementById("math").onclick = function () {
+    var numberOne = parseFloat(document.getElementById("one").value);
+    var numberTwo = parseFloat(document.getElementById("two").value);
+
+    var elPlus = document.getElementById("plus");
+    var elMinus = document.getElementById("minus");
+    var elMnoz = document.getElementById("mnoz");
+    var elDziel = document.getElementById("dziel");
+    var elModulo = document.getElementById("modulo");
+
+    elPlus.textContent = numberOne + numberTwo;
+    elMinus.textContent = numberOne - numberTwo;
+    elMnoz.textContent = numberOne * numberTwo;
+    elDziel.textContent = numberOne / numberTwo;
+    elModulo.textContent = numberOne % numberTwo;
+}
+document.getElementById("math2a").onclick = function () {
+    var numberOne = Math.floor(Math.random()*20);
+    var numberTwo = Math.floor(Math.random()*20);
+
+    var elPlus = document.getElementById("plus2a");
+    var elMinus = document.getElementById("minus2a");
+    var elMnoz = document.getElementById("mnoz2a");
+    var elDziel = document.getElementById("dziel2a");
+    var elModulo = document.getElementById("modulo2a");
+
+    elPlus.textContent = numberOne + numberTwo;
+    elMinus.textContent = numberOne - numberTwo;
+    elMnoz.textContent = numberOne * numberTwo;
+    elDziel.textContent = numberOne / numberTwo;
+    elModulo.textContent = numberOne % numberTwo;
+}
+
