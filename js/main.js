c437ff885544a92ee185ddf243655867fa9e29be
Krzysztof Wolkowycki <wolkowycki.k@gmail.com>
2018-02-07 20:04:58 +0100

    Luck_number_from_to

--- js/main.js
+++ js/main.js
@@ -42,3 +42,11 @@ document.getElementById("throw").onclick = function () {
     var elNumber = document.getElementById("numberHere");
     elNumber.innerHTML = number;
 }
+var numberFrom = parseFloat(document.getElementById("from").value);
+var numberTo = parseFloat(document.getElementById("to").value);
+document.getElementById("throw2").onclick = function (numberFrom, numberTo) {
+
+    var number = Math.floor(Math.random() * 6) + 1;
+    var elNumber = document.getElementById("numberYour");
+    elNumber.innerHTML = number;
+}
